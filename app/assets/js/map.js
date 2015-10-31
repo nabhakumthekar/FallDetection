eb5d7aa6a2b9b8097b5de18664f86c988a752530
Chun-Hao Lin <chunhaolin16@gmail.com>
2015-10-31 19:42:43 +0800

    Set up contact page

--- app/assets/js/map.js
+++ app/assets/js/map.js
@@ -3,17 +3,17 @@ jQuery(document).ready(function(){
 
     map = new GMaps({
         div: '#map',
-        lat: 51.451573,
-        lng: -2.595008,
+        lat: 37.7833,
+        lng: -122.4167,
     });
     map.addMarker({
-        lat: 51.451573,
-        lng: -2.595008,
-        title: 'Address',      
+        lat: 37.7833,
+        lng: -122.4167,
+        title: 'Address',
         infoWindow: {
-            content: '<h5 class="title">College Green</h5><p><span class="region">Address line goes here</span><br><span class="postal-code">Postcode</span><br><span class="country-name">Country</span></p>'
+            content: '<h5 class="title">FindMe</h5><p><span class="region">Address line goes here</span><br><span class="postal-code">Postcode</span><br><span class="country-name">Country</span></p>'
         }
-        
+
     });
 
 });
