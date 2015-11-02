d7ed26d4d9205f4cc6c9d57b1c40b9367f3de333
Chun-Hao Lin <chunhaolin16@gmail.com>
2015-11-03 00:00:07 +0800

    Update images and Google map locations

--- app/assets/js/map.js
+++ app/assets/js/map.js
@@ -3,12 +3,12 @@ jQuery(document).ready(function(){
 
     map = new GMaps({
         div: '#map',
-        lat: 37.7833,
-        lng: -122.4167,
+        lat: 37.423816,
+        lng: -122.197141
     });
     map.addMarker({
-        lat: 37.7833,
-        lng: -122.4167,
+        lat: 37.423816,
+        lng: -122.197141,
         title: 'Address',
         infoWindow: {
             content: '<h5 class="title">FindMe</h5><p><span class="region">Address line goes here</span><br><span class="postal-code">Postcode</span><br><span class="country-name">Country</span></p>'
