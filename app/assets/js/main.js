822fe5f4bcb9af1e700200026bdb50dc7c61d39a
Chun-Hao Lin <chunhaolin16@gmail.com>
2015-11-18 01:14:43 +0800

    Fix nav bar on the top in tablet version

--- app/assets/js/main.js
+++ app/assets/js/main.js
@@ -64,37 +64,7 @@ $(document).ready(function() {
       interval: 8000
     });
 
-
-    /* ======= Style Switcher ======= */
-    $('#config-trigger').on('click', function(e) {
-        var $panel = $('#config-panel');
-        var panelVisible = $('#config-panel').is(':visible');
-        if (panelVisible) {
-            $panel.hide();
-        } else {
-            $panel.show();
-        }
-        e.preventDefault();
-    });
-
-    $('#config-close').on('click', function(e) {
-        e.preventDefault();
-        $('#config-panel').hide();
-    });
-
-
-    $('#color-options a').on('click', function(e) {
-        var $styleSheet = $(this).attr('data-style');
-		$('#theme-style').attr('href', $styleSheet);
-
-		var $listItem = $(this).closest('li');
-		$listItem.addClass('active');
-		$listItem.siblings().removeClass('active');
-
-		e.preventDefault();
-
-	});
-
+    /* ======== Subscription Function ======== */
     $('button#subscribe').on('click', function(){
     $.ajax({
       url: "/api/v1/subscribers",
