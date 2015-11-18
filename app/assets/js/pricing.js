6527e0de50a3b4e1a73e2fb96dbeac1878f38b51
Chun-Hao Lin <chunhaolin16@gmail.com>
2015-11-19 04:33:19 +0800

    Add validation to pre-order form

--- app/assets/js/pricing.js
+++ app/assets/js/pricing.js
@@ -2,7 +2,24 @@ $(document).ready(function() {
   $('.popup-form').magnificPopup({
 		type: 'inline',
 		preloader: false,
-		focus: '#name'
+		focus: '#name',
+    mainClass: 'mfp-fade',
+    callbacks: {
+      open: function(){
+        $.ajax({
+          url: "/api/v1/data/countries",
+          type: "GET",
+          dataType: "json",
+          success: function(data){
+            var countries = [];
+            $.each( data, function() {
+              countries.push( "<option value='" + this.countryCode + "'>" + this.countryName + "</option>" );
+            });
+            $('#country').append(countries + "</select>");
+          }
+        });
+      }
+    }
 	});
 
   $('.plan1').on('click', function(){
@@ -15,30 +32,47 @@ $(document).ready(function() {
     $plan = '3';
   });
 
-  $(document).on('click', '.preorder-popup-dismiss', function (e) {
-    $.ajax({
-      url: "/api/v1/pre-orders",
-      type: "POST",
-      data: JSON.stringify({'email': $('input#email').val(),
-                            'firstName': $('input#firstName').val(),
-                            'lastName': $('input#lastName').val(),
-                            'dateOfBirth': $('input#DoB').val(),
-                            'phone': $('input#phone').val(),
-                            'addressLine1': $('input#addr1').val(),
-                            'addressLine2': $('input#addr2').val(),
-                            'city': $('input#city').val(),
-                            'stateProvince': $('input#state').val(),
-                            'postalCode': $('input#zip').val(),
-                            'country': $('input#country').val(),
-                            'plan': $plan
-                          }),
-      dataType: "json",
-      contentType: "application/json; charset=utf-8",
-      success: function(result){
-        console.log("Success.");
-      }
-    });
-		e.preventDefault();
-		$.magnificPopup.close();
-	});
+  $('#preorder-form').validate({
+    submitHandler: function() {
+      $.ajax({
+        url: "/api/v1/pre-orders",
+        type: "POST",
+        data: JSON.stringify({'email': $('input#email').val(),
+                              'firstName': $('input#firstName').val(),
+                              'lastName': $('input#lastName').val(),
+                              'dateOfBirth': $('input#DoB').val(),
+                              'phone': $('input#phone').val(),
+                              'addressLine1': $('input#addr1').val(),
+                              'addressLine2': $('input#addr2').val(),
+                              'city': $('input#city').val(),
+                              'stateProvince': $('input#state').val(),
+                              'postalCode': $('input#zip').val(),
+                              'country': $('select#country').val(),
+                              'plan': $plan
+                            }),
+        dataType: "json",
+        contentType: "application/json; charset=utf-8",
+        success: function(result){
+          console.log("Success.");
+        },
+        error: function(result){
+          if(result.status == 409){
+            console.log("Conflict.");
+          }
+        }
+      });
+  		$.magnificPopup.close();
+    },
+    messages: {
+      email: {
+        required: 'Please enter your email'
+      },
+      firstName: {
+        required: 'Please enter your First Name'
+      },
+      lastName: {
+        required: 'Please enter your Last Name'
+      },
+    }
+  });
 });
