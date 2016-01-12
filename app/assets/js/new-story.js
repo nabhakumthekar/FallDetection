f4e70f7ee1493da55aa3b421e42e8639f0d0906c
Chun-Hao Lin <chunhaolin16@gmail.com>
2016-01-13 00:09:43 +0800

    Add "new story" page

---
+++ app/assets/js/new-story.js
@@ -0,0 +1,14 @@
+$(document).ready(function() {
+
+    /* ======= News page masonry ======= */
+    /* Ref: http://desandro.github.io/masonry/index.html */
+
+    var $container = $('#blog-mansonry');
+    $container.imagesLoaded(function(){
+        $container.masonry({
+            itemSelector : '.post'
+        });
+    });
+
+    new WOW().init();
+});
