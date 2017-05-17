c692658cc4c6417f1afa17389321ffea121f889c
Zachary Melancon <zmelan01@gmail.com>
2017-05-17 16:20:25 -0500

    adds news and contact pages

--- alzcarelabs.com/app/assets/js/main.js
+++ alzcarelabs.com/app/assets/js/main.js
@@ -1,105 +1,20 @@
 $(document).ready(function() {
-    /* ======= jQuery Placeholder ======= */
-    $('input, textarea').placeholder();
-
-    /* ======= jQuery FitVids - Responsive Video ======= */
-    $('.video-container').fitVids();
-
-    /* ======= Header Background Slideshow - Flexslider ======= */
-    /* Ref: https://github.com/woothemes/FlexSlider/wiki/FlexSlider-Properties */
-
-    $('#bg-slider').flexslider({
-        animation: 'fade',
-        directionNav: false, //remove the default direction-nav -
-                             // https://github.com/woothemes/FlexSlider/wiki/FlexSlider-Properties
-        controlNav: false, //remove the default control-nav
-        slideshowSpeed: 6000
-    });
-
-    /* ======= FAQ accordion ======= */
-    function toggleIcon(e) {
-        $(e.target)
-            .prev('.panel-heading')
-            .find('.panel-title a')
-            .toggleClass('active')
-            .find('i.fa')
-            .toggleClass('fa-plus-square fa-minus-square');
-    }
-
-    $('.panel').on('hidden.bs.collapse', toggleIcon);
-    $('.panel').on('shown.bs.collapse', toggleIcon);
+    /* ======= Load partials ======= */
+    $('#footerContainer').load('partials/footer.html');
+    $('#subscribeDialogContainer').load('partials/subscribe-dialog.html');
 
     /* ======= Fixed header when scrolled ======= */
-    $('#header').removeClass('navbar-fixed-top');
-
-    $(window).bind('scroll', function() {
+    $(window).on('scroll load', function() {
         if ($(window).scrollTop() > 0) {
-            $('#header').addClass('navbar-fixed-top');
+            $('#header').addClass('scrolled');
         } else {
-            $('#header').removeClass('navbar-fixed-top');
+            $('#header').removeClass('scrolled');
         }
     });
 
-    /* ======= Toggle between Signup & Login & ResetPass Modals ======= */
-    $('#signup-link').on('click', function(e) {
-        $('#login-modal').modal('toggle');
-        $('#signup-modal').modal();
-
-        e.preventDefault();
-    });
-
-    $('#login-link').on('click', function(e) {
-        $('#signup-modal').modal('toggle');
-        $('#login-modal').modal();
-
-        e.preventDefault();
-    });
-
-    $('#back-to-login-link').on('click', function(e) {
-        $('#resetpass-modal').modal('toggle');
-        $('#login-modal').modal();
-
-        e.preventDefault();
-    });
-
-    $('#resetpass-link').on('click', function(e) {
-        $('#login-modal').modal('hide');
-        e.preventDefault();
-    });
-
-    /* ======= Price Plan CTA buttons trigger signup modal ======= */
-    $('#price-plan .btn-cta').on('click', function(e) {
-        $('#signup-modal').modal();
-        e.preventDefault();
-    });
-
-    /* ======= Style Switcher (REMOVE ON YOUR PRODUCTION SITE) ======= */
-    $('#config-trigger').on('click', function(e) {
-        var $panel = $('#config-panel');
-        var panelVisible = $('#config-panel').is(':visible');
-
-        if (panelVisible) {
-            $panel.hide();
-        } else {
-            $panel.show();
-        }
-
-        e.preventDefault();
-    });
-
-    $('#config-close').on('click', function(e) {
-        e.preventDefault();
-        $('#config-panel').hide();
-    });
-
-    $('#color-options a').on('click', function(e) {
-        var $styleSheet = $(this).attr('data-style');
-        $('#theme-style').attr('href', $styleSheet);
-
-        var $listItem = $(this).closest('li');
-        $listItem.addClass('active');
-        $listItem.siblings().removeClass('active');
-
-        e.preventDefault();
+    /* ======= Testimonial Bootstrap Carousel ======= */
+    /* Ref: http://getbootstrap.com/javascript/#carousel */
+    $('#testimonials-carousel').carousel({
+        interval: 8000
     });
 });
