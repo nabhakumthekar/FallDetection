6f59a0771cd5a985169f0e593261c5b1bcbd638f
nabhakumthekar <nabha.kumthekar@gmail.com>
2017-11-07 18:38:34 -0500

    initial commit with correct folder

---
+++ FallDetection/app/src/main/java/com/example/nabha/falldetection/HomeScreenActivity.java
@@ -0,0 +1,42 @@
+package com.example.nabha.falldetection;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.widget.TextView;
+import android.view.View;
+import android.content.Intent;
+import android.database.Cursor;
+import android.net.Uri;
+import android.os.Bundle;
+import android.provider.ContactsContract;
+
+public class HomeScreenActivity extends AppCompatActivity {
+    private static final int CONTACT_RESULT = 100;
+    TextView contact_number;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_home_screen);
+        contact_number = (TextView) findViewById(R.id.select_contact_text);
+    }
+
+    public void selectContact(View v) {
+        Intent selectContactIntent = new Intent(Intent.ACTION_PICK, ContactsContract.CommonDataKinds.Phone.CONTENT_URI);
+        startActivityForResult(selectContactIntent, CONTACT_RESULT);
+    }
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+        try{
+        if (requestCode == CONTACT_RESULT && resultCode == RESULT_OK) {
+            Uri uri = data.getData();
+            Cursor cursor = getContentResolver().query(uri, null, null, null, null);
+            cursor.moveToFirst();
+            int phoneNumberColumn = cursor.getColumnIndex(ContactsContract.CommonDataKinds.Phone.NUMBER);
+            contact_number.setText(cursor.getString(phoneNumberColumn));
+        }
+        }catch(Exception e){
+            e.printStackTrace();
+        }
+    }
+}
